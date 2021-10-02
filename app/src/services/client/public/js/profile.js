99b9bf0de7cf765a60d40ccd10796a4327355005
Vishal Menon <vishalmenon.92@gmail.com>
2021-10-02 22:11:38 +0530

    Updated profile trust list bug

--- app/src/services/client/public/js/profile.js
+++ app/src/services/client/public/js/profile.js
@@ -27,12 +27,12 @@ function  displayProfile(profile, contact_info) {
   // let my_trusted_by =  [];
   const my_trusting = profile.trusting.map((item)=>(item.username));
   const my_trusted_by = profile.trusted_by.map((item)=>(item.username));
-  
+
   (my_trusting.length>0)?
-  document.getElementById("profile_trusting_list").innerHTML = `Trusting : <span class="contact_info">${profile.trusting.toString().replaceAll("," , ", ")}.</span>` :
+  document.getElementById("profile_trusting_list").innerHTML = `Trusting : <span class="contact_info">${my_trusting.toString().replaceAll("," , ", ")}.</span>` :
   document.getElementById("profile_trusting_list").innerHTML = `Trusting : <span class="contact_info">None</span>` ;
   (my_trusted_by.length>0)?
-  document.getElementById("profile_trusted_by_list").innerHTML = `Trusted By : <span class="contact_info">${profile.trusted_by.toString().replaceAll("," , ", ")}.</span>` :
+  document.getElementById("profile_trusted_by_list").innerHTML = `Trusted By : <span class="contact_info">${my_trusted_by.toString().replaceAll("," , ", ")}.</span>` :
   document.getElementById("profile_trusted_by_list").innerHTML = `Trusted By : <span class="contact_info">None</span>`;
 
 }
