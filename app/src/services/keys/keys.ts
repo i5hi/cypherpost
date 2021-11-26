3ba9c6ddf1b5af25200fd437aa6f35315d5e847b
Vishal Menon <vishalmenon.92@gmail.com>
2021-11-26 15:20:36 +0530

    Separated isUnique per object type

--- app/src/services/keys/keys.ts
+++ app/src/services/keys/keys.ts
@@ -30,7 +30,7 @@ export class LionBitKeys implements KeyInterface {
     return store.readMany(usernames);
   }
   async add_recipient_key(username: string, key: Key): Promise<boolean | Error> {
-    const status = await isUnique(username, key);
+    const status = await isUniqueRecipient(username, key);
     if (status instanceof Error) return status;
 
     if (status) {
@@ -48,7 +48,7 @@ export class LionBitKeys implements KeyInterface {
     return store.update_pull(username, UseCase.Recipient, { id });
   }
   async add_profile_key(username: string, key: Key): Promise<boolean | Error> {
-    const status = await isUnique(username, key);
+    const status = await isUniqueProfile(username, key);
     if (status instanceof Error) return status;
     console.log({status});
     if (status) {
@@ -65,7 +65,7 @@ export class LionBitKeys implements KeyInterface {
     return store.update_pull(username, UseCase.Profile, { id });
   }
   async add_post_key(username: string, key: Key): Promise<boolean | Error> {
-    const status = await isUnique(username, key);
+    const status = await isUniquePost(username, key);
     if (status instanceof Error) return status;
 
     if (status) {
@@ -87,7 +87,7 @@ export class LionBitKeys implements KeyInterface {
 // EVERY UPDATE KEY REQUIRES READ FIRST TO CHECK FOR UNIQUE KEY
 // UPDATE WITH DB SPECIFIC CHECK
 
-async function isUnique(username: string, key: Key): Promise<boolean | Error> {
+async function isUniqueRecipient(username: string, key: Key): Promise<boolean | Error> {
 
   try {
     const doc = await store.read(username);
@@ -98,9 +98,49 @@ async function isUnique(username: string, key: Key): Promise<boolean | Error> {
     doc.recipient_keys.map((element) => {
       if (element.key === key.key || element.id === key.id) status = false;
     });
+
+    console.log({status})
+    return status;
+  }
+  catch (e) {
+    handleError({
+      code: 500,
+      message: "isUnique(key) error"
+    })
+  }
+}
+
+async function isUniqueProfile(username: string, key: Key): Promise<boolean | Error> {
+
+  try {
+    const doc = await store.read(username);
+    if (doc instanceof Error) return doc;
+
+    let status = true;
+
     doc.profile_keys.map((element) => {
       if (element.key === key.key || element.id === key.id) status = false;
     });
+
+    console.log({status})
+    return status;
+  }
+  catch (e) {
+    handleError({
+      code: 500,
+      message: "isUnique(key) error"
+    })
+  }
+}
+
+async function isUniquePost(username: string, key: Key): Promise<boolean | Error> {
+
+  try {
+    const doc = await store.read(username);
+    if (doc instanceof Error) return doc;
+
+    let status = true;
+
     doc.post_keys.map((element) => {
       if (element.key === key.key || element.id === key.id ) status = false;
     });
