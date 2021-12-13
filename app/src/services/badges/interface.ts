ab81fccd41020e07a7c168a727c16ed8e4ae11a2
Vishal Menon <vishalmenon.92@gmail.com>
2021-12-14 01:57:38 +0530

    updated badge interface

--- app/src/services/badges/interface.ts
+++ app/src/services/badges/interface.ts
@@ -1,16 +1,16 @@
 export interface TrustBadgeInterface{
   create(from: string, to: string, signature: string): Promise<boolean | Error>;
-  findByTrustGiver(from: string): Promise<Badge[] | Error>;
-  findByTrustReciever(to:string):  Promise<Badge[] | Error>;
+  findByGiver(from: string): Promise<Badge[] | Error>;
+  findByReciever(to:string):  Promise<Badge[] | Error>;
   revoke(from: string, to: string): Promise<boolean | Error>;
 }
 
-export interface TrustBadgeStore{
+export interface BadgeStore{
   create(badge: Badge): Promise<boolean | Error>;
   readByGiver(giver: string): Promise<Badge[] | Error>;
   readByReciever(reciever: string): Promise<Badge[] | Error>;
-  removeByGiver(giver: string): Promise<boolean | Error>;
-  removeByReciever(reciever: string): Promise<boolean | Error>;
+  removeByReciever(reciever: string, type: BadgeType): Promise<boolean | Error>;
+  removeAllByGiver(giver: string): Promise<boolean | Error>;
 }
 
 export interface Badge {
