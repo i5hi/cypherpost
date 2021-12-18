55bed3a7ad3bc7916894622da8b16e6d140149a1
Vishal Menon <vishalmenon.92@gmail.com>
2021-12-18 13:19:53 +0530

    Updated delete_volumes.sh to remove old volume names

--- compose/delete_volumes.sh
+++ compose/delete_volumes.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-docker volume rm      compose_app-keys
-docker volume rm      compose_certbot-etc
-docker volume rm      compose_certbot-var
-docker volume rm      compose_dhparam
-docker volume rm      compose_cp-app-code
-docker volume rm      compose_cp-app-keys
-docker volume rm      compose_cp-client-code
-docker volume rm      compose_cp-client-keys
 docker volume rm      compose_cp-configdb
 docker volume rm      compose_cp-datadb
 docker volume rm      compose_cp-server-code
 docker volume rm      compose_cp-server-keys
 docker volume rm      compose_cp-server-logs
 docker volume rm      compose_cp-web-root
-docker volume rm      compose_web-root
